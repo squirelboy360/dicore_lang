b0ef064f6f83ea39bcd52a6bd5ed824dc7fbc453
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-07 18:27:18 +0100

    created project

---
+++ dicore_lang/dicore_lang/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dicore_lang
+//
+//  Created by Tahiru Agbanwa on 2/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
