9028530b83ef3810aba2303ce56539db6c442275
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-08 17:22:46 +0100

    added token class for simple data types

---
+++ dicore_lang/dicore_lang/compiler/main.cpp
@@ -0,0 +1,7 @@
+//
+//  main.cpp
+//  dicore_lang
+//
+//  Created by Tahiru Agbanwa on 2/8/25.
+//
+
---
+++ dicore_lang/dicore_lang/frontend/token.h
@@ -0,0 +1,28 @@
+//
+//  token.h
+//  dicore_lang
+//
+//  Created by Tahiru Agbanwa on 2/8/25.
+//
+
+#pragma once
+#include <string>
+using namespace std;
+
+enum TokenType{
+    TOK_EOF = 0,
+    TOK_DEF,
+    TOK_IDENTIFIER;
+    TOK_NUMBER,
+    TOK_OPERATOR
+};
+
+class Token{
+public:
+    TokenType type;
+    string lexeme;
+    double numValue;
+    
+    Token(TokenType t, string lex);
+    Token(TokenType t, double val);
+};
